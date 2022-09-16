d019b1e0d3f2b9bfc7e58ca1e1c26d0180c34b07
RobiPoire <54003934+RobiPoire@users.noreply.github.com>
2022-09-16 08:50:46 +0000

    Ajout des exercices 4-5 sur les files

---
+++ 2-StructureDonnéesLinéaires/Files_Ex4.py
@@ -0,0 +1,12 @@
+"""
+Les files - Exercice 4
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Proposer une implémentation des opérations classiques de la file à l'aide des méthodes .pop() 
+et .append() du type liste de Python. Une file vide sera représentée par la liste vide. On pourra 
+se donner arbitrairement une taille maximale pour la file, initialisée dans une variable globale, 
+par exemple MAX = 10. 
+On rappelle que, si F est une liste Python, L.pop(0) retourne l'élément d'indice 0 de F et le 
+supprime de la liste. 
+"""
+
+# flemme
