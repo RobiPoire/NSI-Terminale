9f8a1019d2801001a9f92c84986b08d32c44a88e
RobiPoire <54003934+RobiPoire@users.noreply.github.com>
2022-10-11 14:47:47 +0000

    fibo

--- C4_Récursivité/suite_fibonacci.py
+++ C4_Récursivité/suite_fibonacci.py
@@ -7,6 +7,7 @@ Implémentation de la suite de Fibonacci avec et sans mémorisation
 __author__ = "HabibLebsir, RobiPoire"
 
 import sys
+from time import perf_counter
 sys.setrecursionlimit(1000000000)  # Augmentation de la limite de récursivité
 
 
@@ -43,7 +44,37 @@ def fibo_memo(n: int, memo: dict = {}) -> int:
         return n
 
 
+def fibo_itteration(n: int) -> int:
+    """Calcul de la suite de Fibonacci avec itération
+
+    Args:
+        n (int): le nombre de la suite de Fibonacci
+
+    Returns:
+        int: le nombre de la suite de Fibonacci
+    """
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        a, b = 0, 1
+        for i in range(2, n + 1):
+            a, b = b, a + b
+        return b
+
+
 # Exemples d'utilisation
 if __name__ == "__main__":
-    print(fibo(30))
-    print(fibo_memo(30))
+    debut = perf_counter()
+    print(fibo(35))
+    fin = perf_counter()
+    print(f"Temps d'exécution: {fin - debut} secondes de fibo")
+    debut = perf_counter()  
+    print(fibo_memo(35))
+    fin = perf_counter()
+    print(f"Temps d'exécution: {fin - debut} secondes de fibo_memo")
+    debut = perf_counter()
+    print(fibo_itteration(35))
+    fin = perf_counter()
+    print(f"Temps d'exécution: {round(fin - debut)} secondes de fibo_itteration")
